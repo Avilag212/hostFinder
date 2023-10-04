7c7bb23996035f408b1c52c83170025221bb092c
Gabriel de Avila Farias <gabriel21.farias@outlook.com>
2023-10-04 14:01:36 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,35 @@
+
+[WARNING]: This script was developed solely and exclusively for duly
+authorized penetration testing according
+to the guidelines of each country and state in which it is used, I Gabriel, the author, hereby exempt
+myself from any illegal purpose for which this script is used.
+Use responsibly.
+
+[AVISO]: Este script foi desenvolvido unica e exclusivamente para testes de penetração
+devidamente autorizados segundo as diretrizes de cada país e/ou estado em que é utilizado,
+eu Gabriel, o autor, me isento por meio deste de qualquer fim ilegal
+para qual este script seja utilizado.
+Use com responsabilidade.
+
+[READ THE WARNING BEFORE USE THIS SCRIPT!]:
+How to use:
+Call the script normally: ./hostFinder.py and pass your ip range on this format: 192.168.0.1/24
+Exemple:    ./hostFinder 192.168.0.1/24
+Exemple2:   ./hostFinder 10.0.0.1/24
+Example3:   ./hostFinder 172.16.0.1/20
+
+[LEIA O AVISO ANTES DE USAR O SCRIPT!]
+Como usar:
+Chame o script normalmente: ./hostFinder e passe o seu range de rede como parametro
+neste formato: 192.168.0.1/24
+Exemplo:    ./hostFinder 192.168.0.1/24
+Exemplo2:   ./hostFinder 10.0.0.1/24
+Exemplo3:   ./hostFinder 172.16.0.1/20
+
+[Additional information]:
+This script was developed in Python 3, it can be used in Linux or Windows,
+however in Windows call it with the Python interpreter, like this: python ./findHosts <IP_range>
+
+[Informação adicional]:
+Este script foi desenvolvido em python 3, pode ser utilizando em linux ou em Windows,
+porém em Windows chame com o interpretador python, desta maneira: python ./findHosts <IP_range>
---
+++ hostFinder.py
@@ -0,0 +1,65 @@
+#!/usr/python3
+from scapy.all import ARP, Ether, srp
+import socket
+import re
+import sys
+
+class Finder:
+    def __init__(self,range):
+        regex_padrao = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$'
+        if re.match(regex_padrao, range):
+            self.range = range
+        else:
+            raise ValueError("Invalid argument format!\nFormato de argumento inválido")
+
+    def findHosts(self):
+        ip_range = self.range
+        arp = ARP(pdst=ip_range)
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether/arp
+        result = srp(packet, timeout=3, verbose=0)[0]
+        hosts = {}
+        for sent, received in result:
+            hosts.update({received.psrc:received.hwsrc})
+        
+        self.hosts = hosts
+
+    def showHosts(self):
+        for IP,MAC in self.hosts.items():
+            try:
+                hostname = socket.gethostbyaddr(IP)
+                print(f"[+]Host encontrado: {hostname}")
+            except:
+                print(f"[+]Host encontrado: Unknown hostname")
+            print(f"Endereço lógico: {IP}")
+            print(f"Endereço físico: {MAC}\n")
+
+
+if(__name__ == '__main__'):
+
+    if(len(sys.argv)<2):
+        print("\n")
+        print("============ HOST FINDER ==============")
+        print("         Developed By Avilag           ")
+        print("This script find hosts on LAN with")
+        print("ARP request for broadcast of range")
+        print("specified.")
+        print("=======================================")
+        print("[WARNING]: Read the README.txt before use")
+        print("[AVISO]: Leia o arquivo README.txt antes de usar")
+    else:
+
+        try:
+            finder = Finder(sys.argv[1])
+            finder.findHosts()
+            finder.showHosts()
+        except ValueError as erro:
+            print(erro)
+            print("[WARNING!]: Read the README.txt before use")
+            print("[AVISO]: Leia o arquivo README.txt antes de usar")
+        
+        
+
+
+
+
